38393018b1591133294413bd5aa20e9f9fc98436
aakashverma1124 <info.aakash11@gmail.com>
2020-05-28 09:17:52 +0530

    0 1 Knapsack is added

---
+++ DynamicProgramming/DP02_01Knapsack.java
@@ -0,0 +1,61 @@
+/*
+
+	@author
+	Aakash Verma
+
+	Problem: Given weights and values of n items, put these items in a knapsack of 
+	capacity W to get the maximum total value in the knapsack.
+	In other words:
+	I am given two arrays, one value array and another is, weight array associated with n items respectively.
+	Also, a given int W which is the capacity of the knapsack.
+
+	Task:
+
+	Find out the maximum value subset of val[] such that sum of the weights of this subset 
+	is smaller than or equal to W. 
+
+	Note: You cannot break an item, either pick the complete item or don’t pick it (0-1 property.)
+
+	Approach: Think of the smallest possible scenario.
+	1. When there is no item available. (numberOfItems = 0)
+	2. When item is available but no capacity is there. (numberOfItems = someNumber but capacity = 0)
+
+	We can write it recursivley because we have a termination condition or base condition.
+	
+
+*/
+
+
+
+
+class DP02_01Knapsack {
+
+	public static int knapsack(int value[], int weight[], int capacity, int numberOfItems) {	
+		if(numberOfItems == 0 || capacity == 0) {
+			return 0;
+		}
+
+		/* 	
+			If weight of any item is greater than the capacity we'll directly ignore it.
+			therefore, the same function will be called for n - 1 items available in the array.
+		*/
+		if(weight[numberOfItems - 1] > capacity) {
+			return knapsack(value, weight, capacity, numberOfItems - 1);
+		}
+		else {
+			return Math.max(
+				value[numberOfItems - 1] + knapsack(value, weight, capacity - weight[numberOfItems - 1], numberOfItems - 1),
+					knapsack(value, weight, capacity, numberOfItems - 1));
+		}
+	}
+
+	public static void main(String[] args) {
+		int value[] = {10, 40, 30, 50};
+		int weight[] = {5, 4, 6, 3};
+		int capacity = 10;
+		int numberOfItems = 4;
+		int answer = knapsack(value, weight, capacity, numberOfItems);
+		System.out.println(answer);
+	}
+
+}
